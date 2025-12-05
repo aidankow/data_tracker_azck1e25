bcca5fa8e88a50f8a0d1daa0f1569414a5466515
Aidan Kow <aidankow6@gmail.com>
2025-12-05 21:26:46 +0800

    changed file directory for crontab to work

--- src/tracker.sh
+++ src/tracker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /Users/kuanhuakow/Library/CloudStorage/OneDrive-UniversityofSouthampton/data_tracker_azck1e25/src
+cd /Users/kuanhuakow/comp1314/data_tracker_azck1e25/src
 ./stock-tracker.sh
 ./insert-data.sh
