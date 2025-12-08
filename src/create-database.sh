356ac089abc8a5b17519d6cca9fd85ee18ff58da
Aidan Kow <aidankow6@gmail.com>
2025-12-08 17:59:11 +0800

    restarted crontab + changed data to make currency 4dp in the database

--- src/create-database.sh
+++ src/create-database.sh
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS markets(
 );
 EOF
 
-function create_table_for {
+function create_table1_for {
     $MYSQL -u root my_market_tracker <<EOF
     CREATE TABLE IF NOT EXISTS $1 (
         DataPointID INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,17 +31,29 @@ function create_table_for {
 EOF
 }
 
-create_table_for fbmklci
-create_table_for cpo
-create_table_for sgdmyr
-create_table_for usdmyr
-create_table_for cnymyr
-create_table_for gbpmyr
-create_table_for hkdmyr
-create_table_for twdmyr
-create_table_for jypmyr
-create_table_for nzdmyr
-create_table_for audmyr
-create_table_for eurmyr
-create_table_for cadmyr
-create_table_for chfmyr
+function create_table2_for {
+    $MYSQL -u root my_market_tracker <<EOF
+    CREATE TABLE IF NOT EXISTS $1 (
+        DataPointID INT AUTO_INCREMENT PRIMARY KEY,
+        MarketID VARCHAR(8),
+        Price DECIMAL(10,4),
+        Timestamp DATETIME,
+        FOREIGN KEY (MarketID) REFERENCES Markets(MarketID)
+    );
+EOF
+}
+
+create_table1_for fbmklci
+create_table1_for cpo
+create_table2_for sgdmyr
+create_table2_for usdmyr
+create_table2_for cnymyr
+create_table2_for gbpmyr
+create_table2_for hkdmyr
+create_table2_for twdmyr
+create_table2_for jypmyr
+create_table2_for nzdmyr
+create_table2_for audmyr
+create_table2_for eurmyr
+create_table2_for cadmyr
+create_table2_for chfmyr
